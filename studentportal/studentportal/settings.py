2c4bda5110c679e418ddffa280243bf23156b910
greeshma <greeshmakooderi@gmail.com>
2026-06-09 14:51:26 +0530

    's4'

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'studentapp' / 'static',
+]
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
